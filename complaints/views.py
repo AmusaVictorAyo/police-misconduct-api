01241332c3575f972003dce214d585e4921c7384
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-03-01 16:43:38 -0500

    feat: add filtering, search, and ordering for complaints

--- complaints/views.py
+++ complaints/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from authorities.models import OversightAuthority
 from .models import Complaint, Evidence
