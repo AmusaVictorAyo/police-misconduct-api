94b3c2093de75849c3e5140ec1412a3f04de3b7a
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-03-05 07:48:37 -0500

    Giving users read-access to incidents

--- incidents/views.py
+++ incidents/views.py
@@ -1,7 +1,9 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from .models import Incident
 from .serializers import IncidentSerializer
 
 class IncidentViewSet(ModelViewSet):
-    queryset = Incident.objects.all().order_by("-created_at")
+    queryset = Incident.objects.all()
     serializer_class = IncidentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
