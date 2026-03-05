dda831b88b7bb82aff09448c994154b26a15960e
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-03-05 04:06:16 -0500

    Configure static files for production

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
